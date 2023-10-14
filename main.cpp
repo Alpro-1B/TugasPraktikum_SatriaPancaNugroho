8a64a777d9775d07bd0c2294c3077cc0bb34c53e
ItsSatria29 <satriapanca29@gmail.com>
2023-10-14 21:15:07 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------------------------------------------------" << endl;
+    cout << "Nama : Satria Panca Nugroho" << endl << endl;
+    cout << "NPM : 2310631170152" << endl << endl;
+    cout << "Kelas : B - Informatika" << endl << endl;
+    cout << "Cita - Cita : Programmer" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Ingin Bisa Membuat Program dan Penasaran Tentang AI" << endl;
+    cout << "---------------------------------------------------------------------------------" << endl;
+    return 0;
+}
